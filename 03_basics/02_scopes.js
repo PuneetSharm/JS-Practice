07bd6c898d015b38814ff382dc54060eb4945be3
Puneet Kumar Sharma <spuneet37@gmail.com>
2025-01-20 22:34:29 +0530

    more about scopes when nested

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,10 +6,56 @@ var c = 300;
 if (true) { //block scope
 let a = 18;
 const b = 20;
-console.log("A : ",a);
+// console.log("A : ",a);
 }
 
+//global scope in node and browser are different
 
-
-console.log(a);
+// console.log(a);
 // console.log(b);
+
+function one(){
+    const username = "Puneet";
+
+    function two () { // two can access all the variables 
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website); //gives error website is not defined
+    two();
+}
+
+// one();
+
+if (true) {
+    const username = "Puneet";
+    if(username === "Puneet"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website); //cannot access it. Therefore, gives error
+}
+
+// console.log(username); //cannot access it 
+
+/******interesting ***********/
+
+function addone(num){
+    return num + 1;
+}
+
+addone(5);
+
+const addTwo = function(num){ //function Expression
+    return num + 2;
+}
+
+addTwo(5);
+
+addThree(5); //gives error "Cannot access before initialization"
+//this is part of hoisting
+const addThree = function(num){ //function Expression
+    return num + 2;
+}
+
+// addThree(5);
