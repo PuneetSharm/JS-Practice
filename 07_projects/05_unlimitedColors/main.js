e42bace6ecb2b5748df3e2d692563e09c50346d5
Puneet Kumar Sharma <spuneet37@gmail.com>
2025-02-15 19:14:20 +0530

    Keyboard Check,unlimited Color

---
+++ 07_projects/05_unlimitedColors/main.js
@@ -0,0 +1,31 @@
+//generate a random color 
+
+const randomColor = function () {
+    const hexValue = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++){
+        color = color + hex[Math.floor(math.random() * 16)];
+    }
+    return color;
+}
+
+let intervalId;
+
+const startChangingColor = () => {
+    
+function changeBgColor () {
+    document.body.style.backgroundColor = randomColor();
+}
+
+if(!intervalId){
+    intervalId = setInterval(changeBgColor, 1000);
+    };
+};
+
+const stopChangingColor = () => {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+document.querySelector("#start").addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
